13fe1ac8e539b1023e9d08e38424afed5d3d1101
kevinyun31 <kevinyun31@gmail.com>
2023-07-31 18:41:02 +0900

    복습 후 주석추가

--- src/main/java/shop/mtcoding/blog/repository/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/repository/UserRepository.java
@@ -11,21 +11,22 @@ import shop.mtcoding.blog.dto.JoinDTO;
 
 // BoardController, UserController, UserRepository
 // EntityManger, HttpSesseion -- 스프링이 해준거 IOC
-// 디비와 연결 메모리에 띄우다
+// 디비와 연결하여 메모리에 띄우다
 @Repository
 public class UserRepository {
 
     @Autowired
     private EntityManager em;
+    
+    @Transactional  // 인서트,딜리트,업데이트 등을 할때 트레젝션이 롤백과 커밋을 자동으로 해준다.
+    public void save(JoinDTO joinDTO) {
 
-    @Transactional
-    public void save(JoinDTO joinDTO){
-
-    Query query = em.createNativeQuery("insert into user_tb(username, password, email) values(:username, :password, :email)");
-    query.setParameter("username", joinDTO.getUsername());
-    query.setParameter("password", joinDTO.getPassword());
-    query.setParameter("email", joinDTO.getEmail());
-    query.executeUpdate();
+        Query query = em.createNativeQuery(
+                "insert into user_tb(username, password, email) values(:username, :password, :email)");
+        query.setParameter("username", joinDTO.getUsername());
+        query.setParameter("password", joinDTO.getPassword());
+        query.setParameter("email", joinDTO.getEmail());
+        query.executeUpdate();
     }
 
 }
